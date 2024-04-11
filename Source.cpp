d082605a6545c52cdc9eda43e135be6d28add15f
xReggimon <r-mcmullen.1@onu.edu>
2024-04-11 02:38:36 -0400

    Completed hasDuplicates

--- Source.cpp
+++ Source.cpp
@@ -4,11 +4,11 @@ using namespace std;
 bool isSortedIncreasing(int array[], int size);
 bool isSortedDecreasing(int array[], int size);
 bool hasAdjacentDuplicates(int array[], int size);
-//bool hasDuplicates(int array[], int size);
+bool hasDuplicates(int array[], int size);
 
 int main() {
 
-	int array1[] = { 1,2,2,4,5 };
+	int array1[] = { 1,2,3,2,5 };
 	int array2[] = { 5,4,3,2,1 };
 
 	if (isSortedIncreasing(array1, 5)) {
@@ -30,6 +30,13 @@ int main() {
 	}
 	else {
 		cout << "The data does not have adjecent duplicates" << endl;
+	}
+
+	if (hasDuplicates(array1, 5)) {
+		cout << "The data has duplicates " << endl;
+	}
+	else {
+		cout << "The data does not have duplicates" << endl;
 	}
 }
 
@@ -55,6 +62,17 @@ bool hasAdjacentDuplicates(int array[], int size) {
 	for (int i = 0; i < size - 1; i++) {
 		if (array[i] == array[i + 1]) {
 			return true;
+		}
+	}
+	return false;
+}
+
+bool hasDuplicates(int array[], int size) {
+	for (int i = 0; i < size; i++) {
+		for (int k = i + 1; k < size; k++) {
+			if (array[i] == array[k]) {
+				return true;
+			}
 		}
 	}
 	return false;
