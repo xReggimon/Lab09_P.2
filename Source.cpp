dee4755a5cc85056f34b62173591d818dad0dd6e
xReggimon <r-mcmullen.1@onu.edu>
2024-04-11 02:27:58 -0400

    Completed isSortedIncreasing.

---
+++ Source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+bool isSortedIncreasing(int array[], int size);
+//bool isSortedDecreasing(int array[], int size);
+//bool hasAdjacentDuplicates(int array[], int size);
+//bool hasDuplicates(int array[], int size);
+
+int main() {
+
+	int array1[] = { 1,2,3,4,5 };
+	int array2[] = { 5,4,3,2,1 };
+
+	if (isSortedIncreasing(array2, 5)) {
+		cout << "The data is increasing " << endl;
+	}
+	else {
+		cout << "The data is not increasing" << endl;
+	}
+	
+}
+
+bool isSortedIncreasing(int array[], int size) {
+	for (int i = 0; i < size - 1; i++) {
+		if (array[i] > array[i + 1]) {
+			return false;
+		}
+	}
+	return true;
+}
